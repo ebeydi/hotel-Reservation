231b1da89bcc7ab786909f3e6185194b9e769a30
ebeydi <ebeydif@gmail.com>
2026-03-18 02:39:43 +0000

    Ajout des attributs manquants

--- src/main/java/com/hotel/model/Client.java
+++ src/main/java/com/hotel/model/Client.java
@@ -7,9 +7,9 @@ public class Client extends Users {
     public Client() {}
 
     public Client(String id, String login, String motDePasse, String nom,
-                  String prenom, String email, UsersRole role, String nationalite) {
+                  String prenom,String telephone, String adresse, String email, UsersRole role, String nationalite) {
 
-        super(id, login, motDePasse, nom, prenom, email, role);
+        super(id, login, motDePasse, nom, prenom, telephone, adresse, email, role);
         this.nationalite = nationalite;
     }
 
